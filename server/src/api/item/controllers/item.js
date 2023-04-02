91f972dc5fc74b7076a2346529872045ea22765e
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-04-02 15:00:46 +0100

    Create item.js

---
+++ server/src/api/item/controllers/item.js
@@ -0,0 +1,9 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+/**
+ * item controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::item.item");
